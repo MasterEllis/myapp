662d8e609e09cd320779a9ce54bdf22766ad4cba
ayurso <ayurso@gmail.com>
2025-01-03 19:44:50 +0300

    Кроссовки добавляются в массив закладок

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import { useEffect } from "react";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import Cart from "./components/Cart";
-import axios from"axios";
+import axios from "axios";
 function App() {
+  // открытие/закртие корзины
   const [cartOpened, setCartOpened] = useState(false);
+  // массив всех кроссовок
   const [items, setItems] = useState([]);
+  // массив кроссовок в корзине
   const [cartItems, setCatrItems] = useState([]);
+  // массив кроссовок в закладках
+  const [favoriteItems, setFavoriteItems] = useState([]);
+  //строка поиска
   const [searchValue, setSearchValue] = useState("");
-
+  // инициализируем массивы cartItems и favoriteItems
+  useEffect(() => {}, [cartItems]);
+  useEffect(() => {}, [favoriteItems]);
+  // считываем items с удалённого сервера
   useEffect(() => {
     // fetch("https://6745830b512ddbd807f8533a.mockapi.io/api/sniekers/items")
     //   .then((res) => {
@@ -19,47 +28,70 @@ function App() {
     //   .then((json) => {
     //     setItems(json);
     //   });
-      axios.get("https://6745830b512ddbd807f8533a.mockapi.io/api/sniekers/items")
-        .then((res) => {setItems(res.data); });
-      // axios
-      //   .get(
-      //     "https://6745830b512ddbd807f8533a.mockapi.io/api/sniekers/cartItems"
-      //   )
-      //   .then((res) => {
-      //     setCatrItems(res.data);
-      //   });  
+    axios
+      .get("https://6745830b512ddbd807f8533a.mockapi.io/api/sniekers/items")
+      .then((res) => {
+        setItems(res.data);
+      });
+    // axios
+    //   .get(
+    //     "https://6745830b512ddbd807f8533a.mockapi.io/api/sniekers/cartItems"
+    //   )
+    //   .then((res) => {
+    //     setCatrItems(res.data);
+    //   });
   }, []);
-
-    useEffect(() => {}, [cartItems]);
-
+  // добавление в корзину
   const onAddToCart = (obj) => {
-
     setCatrItems([...cartItems, obj]);
-   
+
     // axios.post(
     //   "https://6745830b512ddbd807f8533a.mockapi.io/api/sniekers/cartItems",
     //   obj
     // );
   };
+  // удаление из корзины
   const onRemoveItem = (id) => {
     // axios.delete(
     //   `https://6745830b512ddbd807f8533a.mockapi.io/api/sniekers/cartItems/${id}`
     // );
     setCatrItems((prev) => prev.filter((item) => item.id_sn !== id));
   };
-
-
-
+  // добавление в закладки
+  const onAddToFavorite = (obj) => {
+    setFavoriteItems([...favoriteItems, obj]);
+    // axios.post(
+    //   "https://6745830b512ddbd807f8533a.mockapi.io/api/sniekers/cartItems",
+    //   obj
+    // );
+  };
+  // удаление из закладок
+  const onRemoveFavorite = (id) => {
+    // axios.delete(
+    //   `https://6745830b512ddbd807f8533a.mockapi.io/api/sniekers/cartItems/${id}`
+    // );
+    setFavoriteItems((prev) => prev.filter((item) => item.id_sn !== id));
+  };
+  // рендеринг главной страницы
   return (
     <div className="wrapper">
       {cartOpened ? (
         <Cart
           onDelete={(id) => onRemoveItem(id)}
           itemsCart={cartItems}
-          onCloseCart={() => {setCartOpened(false); document.querySelector("body").style.overflow = "auto";}}
+          onCloseCart={() => {
+            setCartOpened(false);
+            document.querySelector("body").style.overflow = "auto";
+          }}
         />
       ) : null}
-      <Header onClickCart={() => {setCartOpened(true); document.querySelector("body").style.overflow="hidden" }} />
+      <Header
+        onClickCart={() => {
+          setCartOpened(true);
+          // запрещаем прокрутку страницы
+          document.querySelector("body").style.overflow = "hidden";
+        }}
+      />
       <div className="content">
         <div className="content-title">
           <h1>
@@ -89,13 +121,11 @@ function App() {
               item.title.toLowerCase().includes(searchValue.toLowerCase())
             )
             .map((obj) => (
-              
               <Card
                 key={obj.id_sn}
                 title={obj.title}
                 price={obj.price}
                 imgURL={obj.imgURL}
-              
                 onPlus={() => onAddToCart(obj)}
                 onDel={() => onRemoveItem(obj.id_sn)}
                 isChecked={
@@ -103,6 +133,15 @@ function App() {
                     ? false
                     : true
                 }
+                isFavorit={
+                  favoriteItems.findIndex(
+                    (item) => obj.id_sn === item.id_sn
+                  ) === -1
+                    ? false
+                    : true
+                }
+                onFavorite={() => onAddToFavorite(obj)}
+                onUnFavorite={() => onRemoveFavorite(obj.id_sn)}
               />
             ))}
         </div>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,18 +1,37 @@
 import styles from "./Card.module.scss";
 // import { useState } from "react";
 
-function Card({ imgURL, title, price, onPlus, onDel, isChecked }) {
+function Card({
+  imgURL,
+  title,
+  price,
+  onPlus,
+  onDel,
+  isChecked,
+  isFavorit,
+  onFavorite,
+  onUnFavorite,
+}) {
   // const [isAdded, setIsAdded] = useState(isChecked);
 
   const onClickPlus = () => {
-  isChecked = !isChecked;
-  (isChecked)? onPlus(): onDel();
+    isChecked = !isChecked;
+    isChecked ? onPlus() : onDel();
+  };
+  const onClickFavorit = () => {
+    isFavorit = !isFavorit;
+    isFavorit ? onFavorite() : onUnFavorite();
+    console.log(isFavorit);
   };
 
   return (
     <div className={styles.card}>
       <div className={styles.favorite}>
-        <img src="img/unliked.svg" alt="Нет" />
+        <img
+          onClick={onClickFavorit}
+          src={isFavorit ? "img/liked.svg" : "img/unliked.svg"}
+          alt="Добавить в закладки"
+        />
       </div>
       <img width="100%" src={imgURL} alt="Кроссовки" />
       <h5>{title}</h5>
